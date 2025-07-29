997c29f86470bde577ffeffd4cfd9c0efcfa3026
J-ctrl804 <duboaroslav175@gmail.com>
2025-07-29 14:09:02 +0300

    Create monitor.sh

---
+++ scripts/monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Monitoring server..."
+send_alert() {
+  echo "Sending alert..."
+}
+monitor_server() {
+  while true; do
+    if ! ping -c 1 example.com &> /dev/null; then
+      echo "Server is down!"
+      send_alert
+    fi
+    sleep 60
+  done
+}
+monitor_server
